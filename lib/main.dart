9aabf51a56c4950f49a945206eb7c091e6941d68
Aswin Ranjith <aswin.ranjith@iouring.com>
2024-07-24 01:10:07 +0530

    dependency injection in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutternaut_frontend_webapp/presentation/base_screen/base_screen.dart';
 import 'package:flutternaut_frontend_webapp/theme/light_theme.dart';
+import 'package:flutternaut_frontend_webapp/utils/locator.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FlutternautApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FlutternautApp extends StatefulWidget {
+  const FlutternautApp({super.key});
+
+  @override
+  State<FlutternautApp> createState() => _FlutternautAppState();
+}
+
+class _FlutternautAppState extends State<FlutternautApp> {
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,5 +32,9 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       home: const BaseScreen(),
     );
+  }
+
+  Future<void> init() async {
+    await setupInjector();
   }
 }
