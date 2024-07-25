37a71594c50efe9120345b690bedb59a5c2f8472
Aswin Ranjith <aswin.ranjith@iouring.com>
2024-07-25 17:17:04 +0530

    initial screen changed to home

--- lib/presentation/bloc/root/root_state.dart
+++ lib/presentation/bloc/root/root_state.dart
@@ -3,7 +3,7 @@ part of 'root_bloc.dart';
 class RootState extends Equatable {
   const RootState({
     this.status = RootScreenStatus.initial,
-    this.selectedItem = NavbarType.services,
+    this.selectedItem = NavbarType.home,
   });
 
   final RootScreenStatus status;
