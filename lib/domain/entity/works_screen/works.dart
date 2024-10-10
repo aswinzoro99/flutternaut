b0dd87004119301ba90c70a2d8a098ed6f9f2337
Aswin Ranjith <aswin.ranjith@iouring.com>
2024-10-10 21:59:53 +0530

    works screen changes

--- lib/domain/entity/works_screen/works.dart
+++ lib/domain/entity/works_screen/works.dart
@@ -3,11 +3,13 @@ class Work {
   final String minorDescription;
   final String assetPath;
   final String details;
+  final String projectUrl;
 
   Work({
     required this.projectName,
     required this.minorDescription,
     required this.assetPath,
     required this.details,
+    required this.projectUrl,
   });
 }
